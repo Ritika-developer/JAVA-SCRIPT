f22db5691eaf084559f6f4f3eca701925adefd1a
Ritika-developer <ritikachoudhary6264@gmail.com>
2025-03-07 15:41:21 +0530

    6mar

--- 6mar.js
+++ 6mar.js
@@ -4,7 +4,10 @@ let age = document.querySelector("#age").value
 let email = document.querySelector("#email").value
 
 if(name==""){
-    alert("please input your name")
+    // alert("please input your name")
+    let error = document.querySelector('#errorname')
+    error.innerHTML="enter your name"
+    error.style.color="red"
     document.querySelector('#name').focus()
     return false
 }
@@ -12,14 +15,23 @@ if(name==""){
 else if(age==""){
     alert("please input your age")
     document.querySelector('#age').focus()
-
     return false
+}
+else if(isNaN(age)){
+    alert("please enter a number")
+    document.querySelector('#age').focus()
+    return false
+
 }
 
 else if(email==""){
     alert("please input your email")
     document.querySelector('#email').focus()
-
+    return false
+}
+else if(!(email.includes('@'))){
+    alert('please enter valid email must have @')
+    document.querySelector('#email').focus()
     return false
 }
 }
