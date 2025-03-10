f376a5d5d3f31b01402611e6c2b5cd182b9eded1
Ritika-developer <ritikachoudhary6264@gmail.com>
2025-03-10 15:57:31 +0530

    10mar

--- 10mar.js
+++ 10mar.js
@@ -1,3 +1,20 @@
 let btn = document.querySelector('#btn')
+btn.addEventListener('click',()=>{alert("click")})
 
-btn.addEventListener('click',()=>{alert("hello")})
+
+let btn1 = document.querySelector('#btn1')
+btn1.addEventListener('dblclick',()=>{alert("double click")})
+
+
+let btn2 = document.querySelector('#btn2')
+btn2.addEventListener('mouseenter',()=>{alert("mouse enter")})
+
+
+
+let btn3 = document.querySelector('#btn3')
+btn3.addEventListener('mouseleave',()=>{alert("mouse leave")})
+
+
+
+let btn4 = document.querySelector('#btn4')
+btn4.addEventListener('contextmenu',()=>{alert("context menu")})
