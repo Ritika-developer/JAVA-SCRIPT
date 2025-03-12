b13b91e223b88e492285ef67956e69507bfdaf7e
Ritika-developer <ritikachoudhary6264@gmail.com>
2025-03-12 16:25:54 +0530

    11mar

---
+++ 11mar.js
@@ -0,0 +1,46 @@
+//Q 1
+let head = document.querySelector('#head')
+head.innerHTML="RITIKA"
+
+
+let bd = document.body
+
+//Q2
+function bg(){
+let color = document.getElementsByClassName('.btn')
+bd.setAttribute('bgcolor','red')
+}
+
+
+//Q5
+
+function la(){
+let image = document.querySelector('#image1')
+image.src="image.png"
+}
+
+
+function mo(){
+    let image = document.querySelector('#image1')
+    image.src="1.png"
+    }
+
+
+function ta(){
+    let image = document.querySelector('#image1')
+    image.src='img\download (3).jfif'
+    }
+
+
+function ca(){
+    let image = document.querySelector('#image1')
+    image.src='img\download.jfif'
+    }
+
+function tv(){
+    let image = document.querySelector('#image1')
+    image.src='img\download.jfif'
+    }
+
+
+
--- 12mar.js
+++ 12mar.js
@@ -1,9 +1,40 @@
 
 
-let arr =[ 3,5,2,6,8,2,]
-arr.push("last element")
-console.log(arr);
+// let arr =[ 3,5,2,6,8,2,]
+// //add last element
+// arr.push("second last element")
+// arr.push("last element")
+// console.log(arr);
+
+// //add element in first
+// arr.unshift("first element" , "second element")
+// console.log(arr);
 
 
-arr.unshift("first element")
-console.log(arr);
+
+// //remove last element in an array
+// arr.pop()
+// console.log(arr);
+
+
+
+
+// //remove first element in an array
+// arr.shift()
+// console.log(arr);
+
+
+// beetch kai koi bhi element 
+let arr = ["ritika",20,6545,"bhopal","fsd"]
+let part = arr.slice(1,4)
+console.log(part);
+
+
+let arr1 = [1,2,3,45,6,7,5,89,10,34,76,87]
+let part1 =arr1.slice(2,11)
+console.log(part1);
+for(let i of part1){
+if(part1%2==0){
+    console.log(part1);
+}
+}
