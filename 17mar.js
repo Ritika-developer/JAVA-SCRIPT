67d5133b28d3192b928369fd88f470ecc0dc42cf
Ritika-developer <ritikachoudhary6264@gmail.com>
2025-03-17 16:15:52 +0530

    17mar

--- 17mar.js
+++ 17mar.js
@@ -1,3 +1,24 @@
-let arr=["ritika",20,"saloni",20.0]
-arr.splice(2,1)
-console.log(arr)
+//splice :- element ko remove or add karne kai liye 
+
+// let arr=["ritika",20,"saloni",20.0]
+// arr.splice(2,1,)
+// arr.splice(2,1,"shivani")
+// arr.splice(1,0,"koshal")
+// console.log(arr)
+
+
+
+
+//destructuring  of array :- alag alag  varible 
+
+// let [name,age,shop,ct] = ["cybrom",26,"milan",46] 
+// console.log(shop);
+
+
+
+//rest or spread operator ...   :- merge ka kam karta h 
+// rest use in function 
+let arr1 = ["cybrom",20,"milan",78]
+let arr2 = [ 2,3,5,4]
+let arr3 = [...arr1,...arr2]
+console.log(arr3);
