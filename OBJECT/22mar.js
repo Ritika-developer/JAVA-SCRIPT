3c82fb03ccb8f9345253e17ddecf3506c6a9f3c4
Ritika-developer <ritikachoudhary6264@gmail.com>
2025-03-22 16:24:02 +0530

    js

--- OBJECT/22mar.js
+++ OBJECT/22mar.js
@@ -1,3 +1,4 @@
+//array and function in object
 // let ob = {
 //     name : "ritika",
 //     contact : 7483,
@@ -10,17 +11,64 @@
 // console.log(ob.car[1]);
 
 
+// let ob1 = {
+//     name:"ritika",age:34,contact:432423
+// }
+
+// let ob2 = {
+//     city:"bpl",state:"mp"
+// }
+
+// let ob3 = {
+//     ...ob1,
+//     ...ob2
+// }
+
+// console.log(ob3);
+
+
+
+//Inshort 
 let ob1 = {
     name:"ritika",age:34,contact:432423
 }
-
 let ob2 = {
-    city:"bpl",state:"mp"
+    city:"bpl",state:"mp",...ob1
 }
 
-let ob3 = {
-    ...ob1,
-    ...ob2
+// add element 
+ob2.email = "ritika@gmail.com"
+
+//over write 
+ob2.city = "indore"
+
+//delete object
+delete ob2.state
+console.log(ob2);
+
+//key find karne kai liye
+let person = {
+    name : "ritika",
+    age:20,
+    contact:648732,
+    city:"bpl"
 }
 
-console.log(ob3);
+for(let k in person){
+    console.log(k);
+}
+
+//value print hogi
+for(let k in person){
+    console.log(person[k]);
+}
+
+//VIP INTERVIEW QUESTION
+//Object ek key word h :- yeh ek array print kara kai daita ta 
+//single key print hogi 
+let k = Object.keys(person)
+console.log(k[0]);
+
+//yeh single value print karta h 
+let v = Object.values(person)
+console.log(v[0]);
